e3d794bfc78de4962ea414db472edafcdde28a1d
Зубков Алексей Сергеевич <a.zubkov@owen.ru>
2020-12-02 09:23:28 +0300

    Исправления, т.к. многие тесты стали проваливаться.

--- TDDKata/Calculator.cpp
+++ TDDKata/Calculator.cpp
@@ -31,19 +31,28 @@ int Calculator::Add(char* expression)
     int scanres = sscanf(istr, "//%s\n", tempDelim);
     if (scanres == 1) {
         delim = tempDelim;
-        lencmd = 3 + strlen(delim);
+        lencmd = 3 + strlen(tempDelim);
     }
 
     char* pch = strtok(istr + lencmd, delim);
     while (pch != NULL)                         // пока есть лексемы
     {  
-        len = strlen(pch);
+        int lenLex = strlen(pch);
         char * lexema = strdup(pch);
+
+        if (*lexema == '\n') {
+            return -1;
+        };
+
+        if (*(lexema + lenLex - 1) == '\n') {
+            return -1;
+        }
+
         char* pn = strtok(lexema, "\n");
         while (pn != NULL) {
             char* temp;
-            int arg = strtol(pn, &temp, 0);
-            if (temp != '\0') {
+            int arg = strtol(pn, &temp, 10);
+            if (*temp != '\0') {
                 return -1;
             }
             if ((arg > 9) || (arg < 0)) {
@@ -52,8 +61,13 @@ int Calculator::Add(char* expression)
             result += arg;
             pn = strtok(NULL, "\n");
         }
+        char* temp = pch + lenLex + 1;
 
-        pch = strtok(pch + len + 1, delim);
+        if (temp > istr + len) {
+            break;
+        }
+
+        pch = strtok(pch + lenLex + 1, delim);
     }
 
     return result;
