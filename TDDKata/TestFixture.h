bf7405a74c7c0e0b807b9857e2de7a85f142f0bf
Зубков Алексей Сергеевич <a.zubkov@owen.ru>
2020-12-02 10:37:53 +0300

    Добавил файлы

---
+++ TDDKata/TestFixture.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "pch.h"
+#include "Calculator.h"
+
+class TestFixture: public ::testing::Test
+{
+
+protected:
+	Calculator* calc;
+	void SetUp() override
+	{
+		calc = new Calculator();
+	}
+
+	void TearDown() override
+	{
+		delete calc;
+	}
+
+};
+
