ca20fb5927b6341439f2e8c57eec0c484734f4f3
Зубков Алексей Сергеевич <a.zubkov@owen.ru>
2020-12-02 10:34:36 +0300

    Переделка под TestFixture

--- TDDKata/test.cpp
+++ TDDKata/test.cpp
@@ -1,118 +1,101 @@
 #include "pch.h"
 #include "Calculator.h"
-	TEST(TDDKata, Add1ArgTest) {
+#include "TestFixture.h"
+TEST_F(TestFixture, Add1ArgTest) {
 		char* str = "5";
 		int expected = 5;
-		Calculator myCalculator;
-
-		int actual = myCalculator.Add(str);
+		int actual = calc->Add(str);
 		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, Add2ArgTest) {
+	TEST_F(TestFixture, Add2ArgTest) {
 		char* str = "5,4";
 		int expected = 9;
-		Calculator myCalculator;
-
-		int actual = myCalculator.Add(str);
+		int actual = calc->Add(str);
 		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, Add3ArgTest) {
+	TEST_F(TestFixture, Add3ArgTest) {
 		char* str = "5,4,1";
 		int expected = 10;
-		Calculator myCalculator;
-
-		int actual = myCalculator.Add(str);
+		int actual = calc->Add(str);
 		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, AddSeparatorTest1) {
+	TEST_F(TestFixture, AddSeparatorTest1) {
 		char* str = "5,41";
 		int expected = -1;
-		Calculator myCalculator;
-
-		int actual = myCalculator.Add(str);
+		int actual = calc->Add(str);
 		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, AddSeparatorTest2) {
+	TEST_F(TestFixture, AddSeparatorTest2) {
 		char* str = "54,1";
 		int expected = -1;
-		Calculator myCalculator;
-
-		int actual = myCalculator.Add(str);
+		int actual = calc->Add(str);
 		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, AddSeparatorTest3) {
+	TEST_F(TestFixture, AddSeparatorTest3) {
 		char* str = "54";
 		int expected = -1;
-		Calculator myCalculator;
-
-		int actual = myCalculator.Add(str);
+		int actual = calc->Add(str);
 		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, AddEmpty) {
+	TEST_F(TestFixture, AddEmpty) {
 		char* str = "";
 		int expected = 0;
-		Calculator myCalculator;
-
-		int actual = myCalculator.Add(str);
+		int actual = calc->Add(str);
 		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, AddNegativ) {
+	TEST_F(TestFixture, AddNegativ) {
 		char* str = "-1,1";
 		int expected = -2;
-		Calculator myCalculator;
-
-		int actual = myCalculator.Add(str);
+		int actual = calc->Add(str);
 		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, Add4argTest) {
+	TEST_F(TestFixture, Add4argTest) {
 		char* str = "1,1,3,6";
 		int expected = 11;
-		Calculator myCalculator;
-
-		int actual = myCalculator.Add(str);
+		int actual = calc->Add(str);
 		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, Add5argTest) {
+	TEST_F(TestFixture, Add5argTest) {
 		char* str = "1,1,3,6,1";
 		int expected = 12;
-		Calculator myCalculator;
-
-		int actual = myCalculator.Add(str);
+		int actual = calc->Add(str);
 		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, AddNewLineTest) {
+	TEST_F(TestFixture, AddNewLineTest) {
 		char* str = "1,1\n3,6";
 		int expected = 11;
-		Calculator myCalculator;
-
-		int actual = myCalculator.Add(str);
+		int actual = calc->Add(str);
 		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, AddAllDelimTest) {
+	TEST_F(TestFixture, AddAllDelimTest) {
 		char* str = "//;\n1;1\n3;6";
 		int expected = 11;
-		Calculator myCalculator;
-
-		int actual = myCalculator.Add(str);
+		int actual = calc->Add(str);
 		ASSERT_EQ(expected, actual);
 	}
 
-	TEST(TDDKata, AddInvalidArgTest) {
+	TEST_F(TestFixture, AddInvalidArgTest) {
 		char* str = "1,3,6\n";
 		int expected = -1;
-		Calculator myCalculator;
+		int actual = calc->Add(str);
+		ASSERT_EQ(expected, actual);
+	}
 
-		int actual = myCalculator.Add(str);
+	TEST_F(TestFixture, Sample) {
+		char* str = "1,5,6";
+		int expected = 12;
+
+		int actual = calc->Add(str);
 		ASSERT_EQ(expected, actual);
 	}
